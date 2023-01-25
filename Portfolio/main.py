5b2bf3aabb270af60403eacbf4e9b9d4401910f3
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-01-25 05:44:29 +0530

    push

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -49,7 +49,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("Portfolio/style/style.css")
 
 profile_pic = Image.open("Portfolio/Images/profilephoto.jpeg")
 
