d4b0f281f0623d796e1d391c61349f008544afbe
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-01-29 16:46:00 +0530

    update

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -230,11 +230,6 @@ def create_navbar():
 # Create the Streamlit app
 def main():
     create_navbar()
-    embed_component = {'linkedin': """ <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script> 
-    <div class="badge-base LI-profile-badge" data-locale="en_In" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="sanyam-jain-a5a15a220" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/sanyam-jain-a5a15a220?trk=profile-badge">Sanyam Jain</a></div>
-              """}
-    with st.sidebar:
-        components.html(embed_component['linkedin'], height=210)
 
 if __name__ == "__main__":
     main()
